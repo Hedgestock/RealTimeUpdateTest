bde71de387837b0df42995f29151e5d7bec1063a
Hedgestock <arthur.pamart@epita.fr>
2020-04-24 10:39:44 +0200

    WIP

---
+++ RealTimeTest/SSEAdditions/ISseNotificationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lib.AspNetCore.ServerSentEvents;
+
+namespace RealTimeTest.SSEAdditions
+{
+    internal interface ISseNotificationService : IServerSentEventsService
+    {
+    }
+}
--- RealTimeTest/SSEAdditions/SseEmitterService.cs
+++ RealTimeTest/SSEAdditions/SseEmitterService.cs
@@ -9,14 +9,12 @@ namespace RealTimeTest.SSEAdditions
     public class SseEmitterService : ServerSentEventsService, ISseEmitterService
     {
 
-        private readonly IServerSentEventsService _serverSentEventsService;
 
         public SseEmitterService(IServerSentEventsService serverSentEventsService)
         {
-            _serverSentEventsService = serverSentEventsService;
             Program.data.DataChanged += (object sender, EventArgs e) =>
             {
-                _serverSentEventsService.SendEventAsync(Program.data.ToString());
+                serverSentEventsService.SendEventAsync(Program.data.ToString());
             };
         }
     }
