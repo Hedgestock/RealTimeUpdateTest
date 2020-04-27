ea04e2cd80e7f5c1319851f6bdf256101e7acbe3
Hedgestock <arthur.pamart@epita.fr>
2020-04-27 09:53:22 +0200

    Added websockets

--- RealTimeTest/Startup.cs
+++ RealTimeTest/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Lib.AspNetCore.ServerSentEvents;
 using Microsoft.AspNetCore.Builder;
@@ -12,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using RealTimeTest.SSEAdditions;
+
 
 namespace RealTimeTest
 {
@@ -40,6 +44,7 @@ namespace RealTimeTest
 
             app.UseDefaultFiles()
                 .UseStaticFiles()
+                .UseWebSockets()
                 .UseRouting()
                 .UseEndpoints(endpoints =>
                 {
@@ -51,13 +56,50 @@ namespace RealTimeTest
                         var stream = new StreamReader(request.Body);
                         var body = await stream.ReadToEndAsync();
                         Program.data.data = JsonConvert.DeserializeObject<Program.DataHolder>(body).data;
-                        context.Response.StatusCode = 200 ;
+                        context.Response.StatusCode = 200;
                         await context.Response.CompleteAsync();
                     });
                     endpoints.MapGet("/data",
                         async context => { await context.Response.WriteAsync(Program.data.ToString()); });
+                }).Use(async (context, next) =>
+                {
+                    if (context.Request.Path == "/ws-emitter-receiver")
+                    {
+                        if (context.WebSockets.IsWebSocketRequest)
+                        {
+                            WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                            await Echo(webSocket);
+                            return;
+                        }
+
+                        context.Response.StatusCode = 400;
+                        return;
+
+                    }
+                    await next();
+
                 });
 
         }
+
+        private async Task Echo(WebSocket webSocket)
+        {
+            byte[] buffer = new byte[1024 * 4];
+            Program.data.DataChanged += async (object sender, EventArgs e) =>
+            {
+                var str = Program.data.ToString();
+                await webSocket.SendAsync(new ArraySegment<byte>(Encoding.Default.GetBytes(str), 0, str.Length), WebSocketMessageType.Text, true, CancellationToken.None);
+            };
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                Program.data.data = Encoding.Default.GetString(buffer).Replace("\0", string.Empty); ;
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+
+
     }
 }
