7e3b7844777a0eaf3e637f5c4dce1c5d7440c8b0
Hedgestock <arthur.pamart@epita.fr>
2020-04-30 10:21:36 +0200

    Preparing for small talk.

--- Node.js_based_server/server.ts
+++ Node.js_based_server/server.ts
@@ -5,8 +5,8 @@ import fetch from "node-fetch";
 import https from "https";
 
 const agent = new https.Agent({
-  rejectUnauthorized: false
-})
+  rejectUnauthorized: false,
+});
 
 const port = process.argv[2] ?? 3103;
 const app: express.Application = express();
@@ -22,13 +22,23 @@ const es = new EventSource(
 let version = 0;
 let data = "";
 let sseList = [];
+
+
 es.onopen = (e) => {
   console.log(e);
 };
 
 es.onmessage = (e: MessageEvent) => {
+  console.log("message on event source");
   setData(e.data);
   sseList.map((sse) => sse.write(e));
+};
+
+es.onerror = (e: MessageEvent) => {
+  console.log(new Date());
+  console.log(e);
+  console.log(new Error());
+
 };
 
 app.use(function (req: any, res: any, next: any) {
@@ -39,17 +49,22 @@ app.use(function (req: any, res: any, next: any) {
 });
 
 app.get("/sse", function (req, res) {
+  console.log("request on /sse");
+
   const sse = new SseStream(req);
   sse.pipe(res);
   sseList.push(sse);
 });
 
 app.get("/data", function (req: any, res: any) {
+  console.log("request on /data");
+
   res.set("Content-Type", "text/plain");
   res.send({ data, version });
 });
 
 app.post("/update", function (req: any, res: any) {
+  console.log("posted on /update");
   var data = "";
   req.on("data", function (chunk) {
     data += chunk;
@@ -61,7 +76,7 @@ app.post("/update", function (req: any, res: any) {
         "Content-Type": "application/json",
       },
       body: data,
-      agent
+      agent,
     });
   });
   res.send();
@@ -69,12 +84,15 @@ app.post("/update", function (req: any, res: any) {
 
 function setData(rawData) {
   const messageData = JSON.parse(rawData);
+  console.log("setting data", messageData);
+
   version = messageData.version;
   data = messageData.data;
 }
 
 fetch("https://localhost:44330/data", { agent })
   .then((res) => res.text())
-  .then((text) => setData(text));
+  .then((text) => setData(text))
+  .catch(() => console.error("could not fetch data from server"));
 
 app.listen(port, () => console.info(`Server is listening at port ${port}`));
